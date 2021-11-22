3fddbc07de8b236f623e37efe7f96bb1b95263cb
rohan-ynwa <rohanadla8@gmail.com>
2021-11-21 22:00:05 -0500

    added menu option selector

---
+++ index.js
@@ -0,0 +1,20 @@
+// const buttons = document.querySelectorAll(".menu");
+
+// document.querySelector(".menu").addEventListener('click',()=>{
+//   active();
+// })
+
+// active = () => {
+//   var current = "";
+//   sections.forEach((section) => {
+//     const sectionTop = section.offsetTop; 
+//     if ((-(document.querySelector("section").getBoundingClientRect().top)) >= (sectionTop - 400)) {
+//       current = section.getAttribute("id"); }
+//     });
+//   nav.forEach(i => {
+//     i.classList.remove("active");
+//     if (i.classList.contains(current)) {
+//       i.classList.add("active");
+//     }
+//   });
+// };
