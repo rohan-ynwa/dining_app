97bfa65e04c160cb9c6ac8e95570a56db26e6764
rohan-ynwa <rohanadla8@gmail.com>
2021-11-22 23:51:00 -0500

    added menu selector

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-// //to select menu (lunch, dinner etc.)
-const buttons = document.querySelectorAll(".meal");
+//current selection
+let currMenu = "";
+let currItem = "";
 
+const buttons = document.querySelectorAll(".meal");
+const selections = document.querySelectorAll(".item");
+
+//to select menu (lunch, dinner etc.)
 document.querySelectorAll('.meal').forEach(function(e) {
     e.addEventListener('click', function() {
         buttons.forEach(i => {i.classList.remove("active")})
+        currMenu = this.id;
         this.classList.add("active");
     })
   });
+
+//to select menu (lunch, dinner etc.)
+document.querySelectorAll('.item').forEach(function(e) {
+    e.addEventListener('click', function() {
+        selections.forEach(i => {i.classList.remove("item-active")})
+        currItem = this.id;
+        this.classList.add("item-active");
+    })
+  });
+
+//form stays in current state
+let form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
