028978a81bd7ffdc079e3b1f96b95b6b059d06b0
rohan-ynwa <rohanadla8@gmail.com>
2021-12-01 21:21:04 -0500

    added script to support interactive map

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 //current selection
 let currMenu = "";
 let currItem = "";
+let currStation = "";
 
 const buttons = document.querySelectorAll(".meal");
 const selections = document.querySelectorAll(".item");
+const stations = document.querySelectorAll(".station");
 
 //to select menu (lunch, dinner etc.)
 document.querySelectorAll('.meal').forEach(function(e) {
     e.addEventListener('click', function() {
-        buttons.forEach(i => {i.classList.remove("active")})
+        buttons.forEach(i => {i.classList.remove("menu-active")})
         currMenu = this.id;
-        this.classList.add("active");
+        this.classList.add("menu-active");
     })
   });
 
-//to select menu (lunch, dinner etc.)
+//to select item on menu 
 document.querySelectorAll('.item').forEach(function(e) {
     e.addEventListener('click', function() {
         selections.forEach(i => {i.classList.remove("item-active")})
@@ -23,8 +25,19 @@ document.querySelectorAll('.item').forEach(function(e) {
     })
   }); 
 
+//to select item on menu 
+document.querySelectorAll('.station').forEach(function(e) {
+    e.addEventListener('click', function() {
+        stations.forEach(i => {i.classList.remove("station-active")})
+        currStation = this.id;
+        this.classList.add("station-active");
+        document.getElementById('station-head').innerHTML = "Menu at " + currStation;
+    })
+  }); 
+
 //form stays in current state
 let form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 })
+
